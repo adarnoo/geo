88d298091f3cfd2c4c75f441057696ed9b35cdbf
Adriano Orlando <adrianoorlando04@gmail.com>
2023-02-20 16:19:03 +1300

    slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,8 @@
-// Video toggle
-$('body').on('click', '#ts-video-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-pop-up-container").toggleClass("ts-pop-up-container--close");
-});
-
-// Contact toggle
-$('body').on('click', '#ts-contact-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-contact-screen").toggleClass("ts-contact-screen--show");
-  $(".ts-scroll").toggleClass("ts-scroll--open");
-});
-
-$('.ts-slider').slick({
+$('.ge-slider').slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  prevArrow: $(".ts-prev"),
-  nextArrow: $(".ts-next"),
-  speed: 0
+  speed: 800,
+  autoplay: true,
+  arrows: false
 });
-
-// Video player
-function playVideo() {
-        $('#ts-emma-taylor-video').trigger('play');
-    }
-    function pauseVideo() {
-        $('#ts-emma-taylor-video').trigger('pause');
-    }
